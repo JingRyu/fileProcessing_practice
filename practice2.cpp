a91bbd26584fe95f14281e474d4529305971a608
JingRyu <81526024+JingRyu@users.noreply.github.com>
2021-04-08 18:17:56 -0400

    openFile>>temp stream. while(! ifstream)

---
+++ practice2.cpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void openInputFile(ifstream& openFile)
+{
+    string fileName;
+    cout<<"Please input filename: ";
+    cin>>fileName;
+    openFile.open(fileName);
+
+    while(!openFile)
+    {
+        cout<<"Wrong filename!"<<endl;
+        cout<<"Print again: ";
+        cin>>fileName;
+        openFile.clear();
+        openFile.open(fileName);
+    }
+}
+
+int main(){
+    ifstream openFile;
+    openInputFile(openFile);
+    vector<int> v;
+    int temp;
+
+    while(openFile>>temp){
+        v.push_back(temp);
+    }
+
+    int sum = 0;
+    for(int i:v)
+        sum += i;
+
+        cout<<sum/v.size();
+
+    return 0;
+}
